6a68f90a1737555a3583af0287cb712eddfdf8da
Gracielle Fabri <fabrigracielle7@gmail.com>
2024-11-25 23:26:29 -0300

    Portifólio.HTML,CSS,JAVASCRIPT

---
+++ main.js
@@ -0,0 +1,15 @@
+document.getElementById('meuformulario').addEventListener
+    ('submit', function (e) {
+        e.preventDefault();
+
+
+        const nome = document.getElementById('nome').value;
+        const email = document.getElementById('email').value;
+        const mensagem = document.getElementById('mensagem').value;
+
+        if (nome && email && mensagem) {
+            document.getElementById('meuformulario').reset();
+        } else {
+            alert('Por favor, preencha todos os campos!');
+        }
+    });
